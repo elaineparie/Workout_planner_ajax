1dfeef652887c91be5d6e98945986aa69fc1edb6
github username <github email address>
2018-08-20 21:03:46 -0400

    fixes exercise form options

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -19,6 +19,7 @@ class ExercisesController < ApplicationController
   end
 
   def create
+    binding.pry
     @exercise = Exercise.new(exercise_params)
       if @exercise.save
         redirect_to exercise_path(@exercise)
