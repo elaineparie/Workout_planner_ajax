dc4df7071593453fc1ef46384fc766525f3d92a5
github username <github email address>
2018-09-18 09:30:03 -0400

    reset form after submit

--- app/assets/javascripts/exercises.js
+++ app/assets/javascripts/exercises.js
@@ -3,6 +3,10 @@ $(document).on("turbolinks:load", function () {
     event.preventDefault();
     var values = $(this).serialize();
     $.post('/exercises.json', values).done(function(data) {
+      $("#exercise_name").val("")
+      $("#exercise_sets").val("")
+      $("#exercise_reps").val("")
+      $("#exercise_lbs").val("")
       var exercise = data;
        $("#exerciseName").text("Name: " + exercise["name"]);
        $("#exerciseSet").text("Sets: " + exercise["sets"]);
