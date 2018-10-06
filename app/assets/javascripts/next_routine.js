a492bd553aaa310d00813e7b0427a43700e22c60
github username <github email address>
2018-10-06 09:09:35 -0400

    use js object to append exercises to JS show pages

--- app/assets/javascripts/next_routine.js
+++ app/assets/javascripts/next_routine.js
@@ -1,3 +1,23 @@
+function Exercise(id, name, sets, reps, lbs){
+  this.id = id
+  this.name = name
+  this.sets = sets
+  this.reps = reps
+  this.lbs = lbs
+
+  Exercise.prototype.format = function() {
+    html = '<div class="exerciseList">'
+    html += '<p class="exerciseName">' + this.name + ":" + "</p>"
+    html += '<a href=/exercises/' + this.id +'>View Exercise</a>'
+    html += '<p class="exerciseSets">' + "Sets: " + this.sets + "</p>"
+    html += '<p class="exerciseReps">' + "Reps: " + this.reps + "</p>"
+    html += '<p class="exerciseLbs">' + "Weight: " + this.lbs + "</p><br>"
+
+    html += '</div>'
+    return html
+ }
+}
+
 
 document.addEventListener("turbolinks:load", function() {
   var memberId = parseInt($(".js-next").attr("data-member-id"));
@@ -36,25 +56,32 @@ document.addEventListener("turbolinks:load", function() {
       } else {
         $(".js-next").show()
       var nextIndex = routineIndex + 1
-      var prevIndex = routineIndex
 
       var nextRoutine = data[nextIndex]
-      var prevRoutine = data[prevIndex]
       var nextId = nextRoutine.id
-      var prevId = prevRoutine.id
       var routine = data[nextIndex]
       $(".routineName").text(routine["name"]);
       $(".routineKind").text(routine["kind"]);
       $(".delete-routine").val("Delete " + routine["name"]);
+      // clear exerciseContainer
+      $('#exerciseContainer2').hide()
+      $(".js-next").attr("data-id", nextId);
       debugger
+          $("#exerciseContainer").empty()
       routine["exercises"].forEach (function(exercise) {
-          $("#exerciseName").text(exercise["name"])
-          $("#link").html(`<a href=/routines/${routine.id}/exercises/${exercise.id}>View Exercise</a>`)
+        var exercise = new Exercise(exercise["id"], exercise["name"], exercise["sets"], exercise["reps"], exercise["lbs"])
+        $("#exerciseContainer").append(exercise.format())
+
+          // $(".exerciseName").text(exercise["name"])
+          // $(".link").html(`<a href=/routines/${routine.id}/exercises/${exercise.id}>View Exercise</a>`)
+          // $(".exerciseSets").text("Sets: " + exercise["sets"])
+          // $(".exerciseReps").text("Reps: " + exercise["reps"])
+          // $(".exerciseLbs").text("Weight: " + exercise["lbs"])
       })
 
 
 
-      $(".js-next").attr("data-id", nextId);
+
     }
     });
 
