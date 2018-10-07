0a8ce2984e8f93ef1e6bb3524ed35f135aa55bb1
github username <github email address>
2018-10-07 09:03:39 -0400

    ajax exercise saves to routine

--- app/assets/javascripts/next_routine.js
+++ app/assets/javascripts/next_routine.js
@@ -17,6 +17,25 @@ function Exercise(id, name, sets, reps, lbs, routine_id){
     html += '</div>'
     return html
  }
+
+ Exercise.prototype.exercise_form = function() {
+   html = '<div id="exerciseForm">'
+   html += '<form class="new_exercise" action="/exercises" accept-charset="UTF-8" method="post">'
+   html += '<input name="utf8" type="hidden" value="✓">'
+   html += '<input type="hidden" name="authenticity_token" value="ZawYj7LHmiRZfFRC4qfs7r5e405Th3Z3uUgT84zNhajqbt++5SG9cJw/GcRvdC8A0eNNmD10WWwBUlf8e0ysWQ==">'
+   html += '<label for="exercise_name">Name</label><br>'
+   html += '<input type="text" name="exercise[name]" id="exercise_name"><br>'
+   html += '<label for="exercise_sets">Sets</label><br>'
+   html += '<input type="text" name="exercise[sets]" id="exercise_sets"><br>'
+   html += '<label for="exercise_reps">Reps</label><br>'
+   html += '<input type="text" name="exercise[reps]" id="exercise_reps"><br>'
+   html += '<label for="exercise_lbs">Lbs</label><br>'
+   html += '<input type="text" name="exercise[lbs]" id="exercise_lbs"><br>'
+   html += '<input value=' + this.routine_id + ' type="hidden" name="routine_id" id="routine_id">'
+   html += '<input type="submit" name="commit" value="submit" class="submit_button" data-disable-with="submit">'
+   html += '</form></div>'
+   return html
+ }
 }
 
 function Routine(id, name){
@@ -92,42 +111,40 @@ document.addEventListener("turbolinks:load", function() {
       $(".js-next").attr("data-id", nextId);
           $("#exerciseContainer").empty()
       routine["exercises"].forEach (function(exercise) {
-        debugger
         var exercise = new Exercise(exercise["id"], exercise["name"], exercise["sets"], exercise["reps"], exercise["lbs"])
         $("#exerciseContainer").append(exercise.format())
 
-          // $(".exerciseName").text(exercise["name"])
-          // $(".link").html(`<a href=/routines/${routine.id}/exercises/${exercise.id}>View Exercise</a>`)
-          // $(".exerciseSets").text("Sets: " + exercise["sets"])
-          // $(".exerciseReps").text("Reps: " + exercise["reps"])
-          // $(".exerciseLbs").text("Weight: " + exercise["lbs"])
       })
-
+// debugger
       $(".new_exercise").empty()
 
-      var routine = new Routine(routine["id"], routine["name"])
-      $("#exerciseForm").append(routine.exercise_form())
+      // var routine = new Routine(routine["id"], routine["name"])
+      debugger
+      let exercise_two = new Exercise(data["id"], data["name"], data["sets"], data["reps"], data["lbs"], routine.id)
+      routine["exercises"].push(exercise_two)
+       $("#exerciseForm").append(exercise_two.exercise_form())
       $('.new_exercise').submit(function(event) {
           event.preventDefault();
-          debugger
           var id = $(this).data("id");
             var values = $(this).serialize();
           $.post('/exercises.json', values).done(function(data) {
             debugger
+            exercise_two.id = data["id"]
+            exercise_two.name = data["name"]
+            exercise_two.sets = data["sets"]
+            exercise_two.reps = data["reps"]
+            exercise_two.lbs = data["lbs"]
 
 
-      let exercise_two = new Exercise(data["id"], data["name"], data["sets"], data["reps"], data["lbs"], routine.id)
+
           let formattedExercise = exercise_two.format()
         var exerciseList = ""
                 exerciseList += '<li>' + formattedExercise + '</li>'
             $("#exercise_list").append(exerciseList)
 
               })
-
-
-    })
-      }
-        });
+            })
+          }
+      });
   });
-
 })
